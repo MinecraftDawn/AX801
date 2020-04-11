982cee7108af6a87244babab12ce2b61614e3fbd
MinecraftDawn <eric4902077@gmail.com>
2020-04-11 11:53:27 +0800

    class2

---
+++ class1-2/class2/main.c
@@ -0,0 +1,16 @@
+// standard Input Outout
+#include <stdio.h>
+// standard Library
+#include <stdlib.h>
+
+int main(){
+
+    for(int i = 0,j=0; i < 3||j<3;i++){
+        printf("Hello \n");
+    }
+
+
+
+
+
+}
